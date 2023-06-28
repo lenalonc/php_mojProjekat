4fe5576283db036a94fcb48c002d0e71f686a32d
Lena Loncarevic <loncareviclenaa@gmail.com>
2023-06-28 23:45:10 +0200

    Funkcija logovanja omogucena

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
+<?php
+
+require "dbBroker.php";
+require "model/user.php";
+
+if(isset($_POST['username']) && isset($_POST['password'])){
+  $uid = 1;
+  $un = $_POST['username'];
+  $up = $_POST['password'];
+ 
+
+  $korisnik = new User($uid,$un,$up);
+
+  $odg = User::logInUser($korisnik, $conn);
+
+  //nas odg ako je dobar, treba da ima samo jedan red
+  if($odg->num_rows == 1){
+    echo "Uspesno ste se prijavili";
+
+    $_SESSION['loggeduser'] = "prijavljen";
+    $_SESSION['user_id'] = $korisnik->id;
+    
+    header('Location: home.php');
+    exit();
+  }
+
+  else{
+    echo `
+        <script>
+        console.log("Niste se prijavili!");
+        </script>
+        `;
+  }
+
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,20 +54,20 @@
         <div class="center">
           <img src="img/logoZ-02.png" alt="logo"> 
         </div>
-        <form action="/users/login" style="" class="login-form" id="UserLoginForm" method="post" accept-charset="utf-8">
+        <form method="POST" action="#">
           <div class="control-group">
             <div class="input-prepend">
 
 
               <span class="add-on"><i class="icon-user"></i></span>
-              <input name="username" required="required" placeholder="Username" maxlength="255" type="text" id="UserUsername">
+              <input name="username" required="required" placeholder="Username" maxlength="255" type="text" class="form-control">
               <br>
               <br>
               <span class="add-on"><i class="icon-lock"></i></span>
-              <input name="password" required="required" placeholder="Password" type="password" id="UserPassword"> 
+              <input name="password" required="required" placeholder="Password" type="password" class="form-control"> 
               <br>
               <br>
-            <button class="btn btn-primary btn-large btn-block" type="submit" value="Sign in">Prijavi se</button> 
+            <button class="btn btn-primary btn-large btn-block" type="submit">Prijavi se</button> 
           </div>
         </form>
       </div><!--/.login-->

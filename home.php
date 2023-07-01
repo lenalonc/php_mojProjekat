61578dd2611c54830fa84ca592a003c426935f77
Lena Loncarevic <loncareviclenaa@gmail.com>
2023-07-01 15:55:28 +0200

    Manje izmene za home stranicu

--- home.php
+++ home.php
@@ -5,7 +5,7 @@ require "model/termin.php";
 
 session_start();
 
-if(empty($_SESSION['loggeduser']) || $_SESSION['loggeduser']==''){
+if(empty($_SESSION['loggeduser']) || $_SESSION['loggeduser'] == ''){
     header("Location: index.php");
     die();
 }
@@ -26,8 +26,6 @@ else{
 
 ?>
 
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -46,7 +44,7 @@ else{
         </div>
     </div>
     <img src="img/logo-01.png" alt="logo" class="logo">
-    <div class="col-md-8" style="text-align:center; width:70%; position: fixed; top: 42%; left: 60%; transform: translate(-50%, -50%);">
+    <div class="col-md-4" style="text-align:center; width:70%; position: fixed; top: 42%; left: 60%; transform: translate(-50%, -50%);">
         <div id="pregled">
             <table id="tabela" class="table table-bordered table-dark">
                 <thead>
@@ -88,23 +86,23 @@ else{
             <div class="col-md-4" style="width:30%; position: fixed; bottom: 100px; left: 100px;">
                 
                 <button id="btn" class="btn btn-info btn-block" 
-                style="width: 300px; height: 50px; background-color: #355E3B ; border: 5px solid; border-radius: 1rem; "> Prikazi termine</button>
+                style="width: 300px; height: 50px; background-color: #355E3B ; border: 5px solid; border-radius: 1rem; "  onclick="prikazi()"> Prikazi termine</button>
                 
                 <button id="btn-dodaj" type="button" class="btn btn-success btn-block"
                 style="width: 300px; height: 50px;background-color: #355E3B ; border: 5px solid; border-radius: 1rem;" data-toggle="modal" data-target="#zakaziModal">Zakazi termin</button>
                 
                 <button id="btn-pretraga" class="btn btn-warning btn-block"
                 style="width: 300px; height: 50px;background-color:  #355E3B; border: 5px solid; border-radius: 1rem;" > Pretrazi termin</button>
-                <input type="text" id="myInput" onkeyup="funkcijaZaPretragu()" placeholder="Pretrazi kolokvijume po predmetu" hidden>
+                <input type="text" id="myInput" onkeyup="findBy()" placeholder="Pretrazi termine po predmetu" hidden>
                 
                 <button id="btn-izmeni" type="button" class="btn btn-warning btn-block"
                 style="width: 300px; height: 50px;background-color: #355E3B ; border: 5px solid; border-radius: 1rem;" data-toggle="modal" data-target="#izmeniModal">Izmeni</button>
 
-                <button id="btn-obrisi" class="btn btn-danger btn-block"
+                <button id="btn-obrisi" formmethod="post" class="btn btn-danger btn-block"
                 style="width: 300px; height: 50px;background-color: #355E3B ; border: 5px solid; border-radius: 1rem;">Obrisi termin</button>
                 
                 <button id="btn-sortiraj" class="btn btn-normal btn-block"
-                style="width: 300px; height: 50px;background-color:  #355E3B; border: 5px solid; color: white; border-radius: 1rem;" onclick="sortTable()">Sortiraj</button>
+                style="width: 300px; height: 50px;background-color:  #355E3B; border: 5px solid; color: white; border-radius: 1rem;" onclick="sortTable()">Sortiraj po datumu</button>
 
             </div>
 
@@ -125,26 +123,26 @@ else{
                             <div class="col-md-11 ">
                                 <div class="form-group">
                                     <label for="">Usluga</label>
-                                    <input type="text" style="border: 1px solid black" name="usluga" class="form-control"/>
+                                    <input  type="text" style="width: 400px;border: 1px solid #355E3B" name="usluga" class="form-control"/>
                                 </div>
                                 <div class="form-group">
                                 <label for="">Klijent</label>
-                                    <input type="text" style="border: 1px solid black" name="klijent" class="form-control"/>
+                                    <input type="text" style="width: 400px; border: 1px solid #355E3B" name="klijent" class="form-control"/>
                                 </div>
                                 <div class="form-group">
                                     <label for="cena">Cena</label>
-                                    <input type="cena" style="border: 1px solid black" name="cena" class="form-control"/>
+                                    <input type="cena" style="width: 400px; border: 1px solid #355E3B" name="cena" class="form-control"/>
                                 </div>
                                 <div class="col-md-12">
                                 <div class="form-group">
                                     <label for="">Datum</label>
-                                    <input type="date"  style="border: 1px solid black" name="datum" class="form-control"/>
+                                    <input type="date"  style="width: 400px; margin-left: -15px;border: 1px solid #355E3B" name="datum" class="form-control"/>
                                 </div>
                                 </div>
                                 
                                 <div class="form-group">
                                     <button id="btnDodaj" type="submit" class="btn btn-success btn-block"
-                                    tyle="background-color: rgb(53, 94, 59); border: 1px solid black;">Zakazi</button>
+                                    tyle="background-color: rgb(53, 94, 59); border: 1px solid #355E3B;">Zakazi</button>
                                 </div>
                             </div>
                         </div>
@@ -209,8 +207,69 @@ else{
         </div>
     </div>
 </div>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="js/main.js"></script>
-        </body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+
+    <script>
+
+        function sortTable(){
+            var table, rows, switching, i, x, y, shouldSwitch;
+            table = document.getElementById("tabela");
+            switching = true;
+            while (switching) {
+                switching = false;
+                rows = table.rows;
+                for (i = 1; i < (rows.length - 1); i++) {
+                    shouldSwitch = false;
+                    x = rows[i].getElementsByTagName("TD")[3];
+                    y = rows[i + 1].getElementsByTagName("TD")[3];
+                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+                if (shouldSwitch) {
+                    rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                    switching = true;
+                }
+            }
+
+        }
+
+
+        function findBy() {
+
+            var input, filter, table, tr, i, td1, td2, td4, txtValue1, txtValue2, txtValue4;
+            input = document.getElementById("myInput");
+            filter = input.value.toUpperCase();
+            table = document.getElementById("tabela");
+            tr = table.getElementsByTagName("tr");
+
+            for (i = 0; i < tr.length; i++) {
+                td1 = tr[i].getElementsByTagName("td")[0];
+                td2 = tr[i].getElementsByTagName("td")[1];
+                td4 = tr[i].getElementsByTagName("td")[3];
+
+                if (td1 || td2 || td4) {
+                    txtValue1 = td1.textContent || td1.innerText;
+                    txtValue2 = td2.textContent || td2.innerText;
+                    txtValue4 = td4.textContent || td4.innerText;
+
+                    if (txtValue1.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1 ||
+                         txtValue4.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    } else {
+                     tr[i].style.display = "none";
+                            }
+                 }
+            }
+        }
+
+    </script>
+
+
+
+
+</body>
 </html>

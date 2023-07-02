8b3091b59a850617b83dca881816ef0cb682b5b8
Lena Loncarevic <loncareviclenaa@gmail.com>
2023-07-02 14:23:12 +0200

    Baza cenovnik

--- kozmeticki_salon.sql
+++ kozmeticki_salon.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 28, 2023 at 10:02 PM
+-- Generation Time: Jul 01, 2023 at 04:31 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -20,6 +20,36 @@ SET time_zone = "+00:00";
 --
 -- Database: `kozmeticki_salon`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cenovnik`
+--
+
+CREATE TABLE `cenovnik` (
+  `id` int(11) NOT NULL,
+  `tretman` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `cena` int(16) NOT NULL,
+  `kozmeticar` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cenovnik`
+--
+
+INSERT INTO `cenovnik` (`id`, `tretman`, `cena`, `kozmeticar`) VALUES
+(1, 'Tretman dermaoxy 1', 6000, 'Zorica'),
+(2, 'Tretman dermaoxy 2', 5000, 'Zorica'),
+(5, 'Fitopiling terapija', 5000, 'Zorica'),
+(6, 'Biotretman alge', 4000, 'Zorica'),
+(7, 'Mezoporacija', 3000, 'Zorica'),
+(8, 'Klasican tretman', 4000, 'Zorica'),
+(9, 'Tretman mlade koze', 3000, 'Zorica'),
+(10, 'Oblikovanje obrva', 600, 'Nina'),
+(11, 'Manikir', 1000, 'Nina'),
+(12, 'CND lakiranje', 1600, 'Nina'),
+(13, 'Depilacija', 1000, 'Nina');
 
 -- --------------------------------------------------------
 
@@ -59,19 +89,22 @@ CREATE TABLE `zakazani_termini` (
 --
 
 INSERT INTO `zakazani_termini` (`id`, `usluga`, `klijent`, `cena`, `datum`) VALUES
-(1, 'Fitopiling terapija', 'Zorica Markovic', '4000', '2023-07-07'),
-(4, 'Oblikovanje obrva', 'Mira Stankovic', '500', '2023-07-03'),
-(5, 'Manikir', 'Danica Djuric', '1000', '2023-07-12'),
-(6, 'Tretman vocne kiseline', 'Milos Lazic', '5000', '2023-07-11'),
-(7, 'Tretman mlade koze', 'Darko Gavrilovic', '3000', '2023-07-17'),
-(8, 'Tretman vocne kiseline', 'Milos Lazic', '5000', '2023-07-11'),
-(9, 'Tretman mlade koze', 'Darko Gavrilovic', '3000', '2023-07-17'),
-(10, 'Nadogradnja trepavica', 'Milica Radic', '2000', '2023-07-11'),
-(11, 'Tretman dermaoxy 1', 'Darko Gavrilovic', '6000', '2023-07-13');
+(1, 'Manikir', 'Danica Djuric', '1000', '2023-06-30'),
+(2, 'Fitopiling terapija', 'Mika Mikic', '4000', '2023-06-30'),
+(4, 'Tretman mlade koze', 'Darko Gavrilovic', '3000', '2023-07-03'),
+(5, 'Tretman dermaoxy 1', 'Marko Ivanovic', '6000', '2023-07-05'),
+(26, 'Tretman mlade koze', 'Milos Milovanovic', '3000', '2023-07-04'),
+(33, 'Mila', 'Manikir', '433', '2023-07-03');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `cenovnik`
+--
+ALTER TABLE `cenovnik`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
@@ -90,6 +123,12 @@ ALTER TABLE `zakazani_termini`
 --
 
 --
+-- AUTO_INCREMENT for table `cenovnik`
+--
+ALTER TABLE `cenovnik`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
@@ -99,7 +138,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `zakazani_termini`
 --
 ALTER TABLE `zakazani_termini`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
